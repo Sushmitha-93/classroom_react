20c5b5fac0ccd0f9fa84e8ee79e309a20f724187
Sushmitha DT <dtsushmitha@gmail.com>
2019-11-12 00:04:38 +0530

    Student service

---
+++ src/services/studentServices.js
@@ -0,0 +1,25 @@
+import Axios from "axios";
+
+Axios.defaults.baseURL = "https://localhost:3000/api";
+
+export function getStudents() {
+  return Axios.get("/students");
+}
+
+export function deleteStudent(movieId) {
+  return Axios.delete("/students/" + movieId);
+}
+
+export function saveStudent(student) {
+  // if '_id' property is there, then its a PUT request otherwise its POST request.
+  if (student._id) {
+    return Axios.put("/students/" + student._id, {
+      name: student.name,
+      rollno: student.rollno,
+      class: student.class,
+      phone: student.phone,
+      gender: student.gender,
+      address: student.address
+    });
+  } else return Axios.post("/students/", student);
+}

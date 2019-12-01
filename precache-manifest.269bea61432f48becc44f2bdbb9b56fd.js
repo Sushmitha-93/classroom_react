68a7ca3e0f4b66328a2ece01ed5ed56c6b64f44b
Sushmitha DT <dtsushmitha@gmail.com>
2019-12-01 12:46:44 +0530

    Updates

---
+++ precache-manifest.269bea61432f48becc44f2bdbb9b56fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "603b24b614bbfb6a22c27c252b697ece",
+    "url": "/classroom_react/index.html"
+  },
+  {
+    "revision": "111eb974e72e4f444d25",
+    "url": "/classroom_react/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "5bde0bdf6ab09644f85b",
+    "url": "/classroom_react/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "111eb974e72e4f444d25",
+    "url": "/classroom_react/static/js/2.42d4b3d1.chunk.js"
+  },
+  {
+    "revision": "5bde0bdf6ab09644f85b",
+    "url": "/classroom_react/static/js/main.3c9f810d.chunk.js"
+  },
+  {
+    "revision": "98f190d1e9a0b6c5b4b6",
+    "url": "/classroom_react/static/js/runtime-main.cde8300a.js"
+  }
+]);
